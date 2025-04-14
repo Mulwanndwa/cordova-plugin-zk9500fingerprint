2ca0e7f9cc6cf67959357f6c282dddf6236559aa
Mpho Mulwanndwa <mulwanndwa.mpho@gmail.com>
2025-04-14 14:12:40 +0200

    Update zktecoFinger.js
    Signed-off-by: Mpho Mulwanndwa <mulwanndwa.mpho@gmail.com>

--- www/zktecoFinger.js
+++ www/zktecoFinger.js
@@ -3,12 +3,12 @@ var exec = require('cordova/exec');
 //@ enroll a fingerprint
 exports.scan = function(success,error)
 {
-    exec(success,error,"zk9500","scan",[]);
+    exec(success,error,"zktecoFinger","scan",[]);
 }
 
 exports.custom = function(filePath,fileContent,success,error)
 {    
-    exec(success,error,"zkFinger","saveTemplate", [filePath,fileContent]);
+    exec(success,error,"zktecoFinger","saveTemplate", [filePath,fileContent]);
 }
 
 // function success(data)
